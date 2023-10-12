7f3324fa61ca8306133b6d922036eaad19db5c04
yuukikawano <heyeyuxi9@gmail.com>
2023-10-12 15:21:20 +0900

    A

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -53,13 +54,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
+   
 
     def correct_user
       @user = User.find(params[:id])
